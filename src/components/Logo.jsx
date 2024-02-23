951658883c37cdf40680a04f8164747ad0bfb1cb
Vansh Gupta <97226346+goyalvansh4@users.noreply.github.com>
2024-02-23 11:53:14 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '50px'}) {
   return (
     <div>
-      <img src='https://github.com/goyalvansh4/MyBlogProject/blob/main/public/logo.png' alt='logo' width={width} />
+      <img src='./logo.png' alt='logo' width={width} />
     </div>
   )
 }
